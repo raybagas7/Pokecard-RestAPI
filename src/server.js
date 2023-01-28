b73056855ef4025a84ff0924b9112fe3640a04d7
Ray <raysam.gas@gmail.com>
2023-01-29 04:02:02 +0700

    disbale cors

--- src/server.js
+++ src/server.js
@@ -76,11 +76,11 @@ const init = async () => {
   const server = Hapi.server({
     port: config.app.port,
     host: process.env.NODE_ENV !== 'production' ? config.app.host : '0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
+    // routes: {
+    //   cors: {
+    //     origin: ['*'],
+    //   },
+    // },
   });
 
   // Tambah plugin eksternal
