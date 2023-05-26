de1bbbf6d0619e459cfef3a37ff74ee9b46b12e6
raybagas7 <raysam.gas@gmail.com>
2023-05-26 14:49:25 +0700

    fix firebaseConfig

--- src/services/firebase/StorageService.js
+++ src/services/firebase/StorageService.js
@@ -21,7 +21,6 @@ class StorageService {
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.FIREBASE_APP_ID,
       measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     };
     const metadata = {
       contentType: meta.headers['content-type'],
